26acb1d6e634343b69a331501e9b543068da0b3f
Lyubov Kovalchuk <66961026+lyubakek@users.noreply.github.com>
2022-01-30 23:29:40 +0200

    add faster solution

--- ThirdMaximumNumber.swift
+++ ThirdMaximumNumber.swift
@@ -14,3 +14,25 @@ class Solution {
         return max
     }
 }
+
+
+// Runtime: 24 ms, faster than 99.31% of Swift online submissions for Third Maximum Number.
+
+class Solution {
+    func thirdMax(_ nums: [Int]) -> Int {
+        var numsCopy = Set(nums)
+        
+        if numsCopy.count < 3 {
+            return numsCopy.max()!
+        }
+        
+        if numsCopy.count == 3 {
+            return numsCopy.min()!
+        }
+        
+        for i in 0...1 {
+            numsCopy.remove(numsCopy.max()!)
+        }
+        return numsCopy.max()!
+    }
+}
